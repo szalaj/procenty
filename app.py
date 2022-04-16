d70cc7b089ed6c0f358081df0a4193e7425e3f85
szalaj <mszalajski@gmail.com>
2022-04-16 12:32:24 +0200

    app

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def main():
 
+    # tu po wypełnieniu i przesłaniu formularza
     if request.method == "POST":
 
         req = request.form
@@ -26,18 +27,18 @@ def main():
             return render_template("main.html", feedback=feedback)
 
 
+        # zmienne
+        # pozyczony kapital
         N = float(req["kapital"])
+
+        # oprocentowanie
         r = float(req["procent"])/100
+
+        # liczba rat
         n = int(req["liczba_rat"])
 
 
         print("--obliczenia--")
-
-        # pożyczona kwota - kapitał
-        #N = 460000
-
-        # oprocentowanie w skali roku
-        #r = (2.2 + 0 + 5.97)/100
 
         # liczba rat płatnych w ciągu roku
         k = 12
@@ -54,6 +55,7 @@ def main():
 
         return render_template("main.html", feedback="rata równa : {}".format(I))
 
+    # tu zaczyna się aplikacja
     return render_template("main.html")
 
 
