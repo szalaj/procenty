398c79dd4041a648b0dac577abde8965476378a3
szalaj <mszalajski@gmail.com>
2022-04-16 14:10:24 +0200

    d

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def main():
 
         I = round(I, 2)
 
-        dane = {'kapital':N, 'zwrot':n*I, 'rata': I}
+        dane = {'kapital':N, 'zwrot':round(n*I,2), 'rata': I}
 
         return render_template("main.html", dane=dane)
 
