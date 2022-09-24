f980f958aafc28b137fe7b6294068222ffa6292f
szalaj <mszalajski@gmail.com>
2022-09-24 23:44:50 +0200

    argv sys

--- proc.py
+++ proc.py
@@ -1,8 +1,18 @@
 import yaml
+import sys
+import getopt
+import argparse
 if __name__== "__main__":
     
-    
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("echo")
+    parser.add_argument("-v", "--verbosity", type=int,
+                    help="increase output verbosity")
+    args = parser.parse_args()
+    print(args.echo)
+
+    
     stream = open("./models/mod1.yml", 'r')
     dictionary = yaml.safe_load(stream)
     for key, value in dictionary.items():
