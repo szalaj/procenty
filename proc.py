57ccfd631902114ccf1949902d6127a263bd7d16
szalaj <mszalajski@gmail.com>
2022-10-03 22:01:12 +0200

    co

--- proc.py
+++ proc.py
@@ -3,16 +3,7 @@ import sys
 import getopt
 import argparse
 if __name__== "__main__":
-    
 
-    #parser = argparse.ArgumentParser()
-    #parser.add_argument("echo")
-    #parser.add_argument("-v", "--verbosity", type=int,
-    #               help="increase output verbosity")
-    #args = parser.parse_args()
-    #print(args.echo)
-
-    
     stream = open("./models/mod1.yml", 'r')
     dane = yaml.safe_load(stream)
     for key, value in dane.items():
@@ -22,7 +13,7 @@ if __name__== "__main__":
     p = dane['p']/100.0
     S = dane['K']
     L = (S*p)
-    M = k*(1-pow(k/(k+p),360) )
+    M = k*(1-pow(k/(k+p),360))
     I = L/M
 
     print('I : {}'.format(I))
