0963601955632fb8d4dfcd1ab901d72943dcff80
szalaj <mszalajski@gmail.com>
2022-11-16 21:53:42 +0100

    get oprc

--- generate_model.py
+++ generate_model.py
@@ -3,6 +3,7 @@ import sys
 import getopt
 import argparse
 import datetime as dt
+import time
 from dateutil.relativedelta import relativedelta
 from collections import OrderedDict
 
@@ -28,8 +29,33 @@ class Oprocentowanie:
         self.d1b = d1b
         self.d2 = d2
 
+        delta_d1 =  time.mktime(d1a.timetuple()) - time.mktime(d0.timetuple())
+        delta_p1 = p1 - p0
+
+        self.a1 = delta_p1/delta_d1
+        self.b1 = p0 - d0*self.a1
+
+        delta_d2 =  time.mktime(d2.timetuple()) - time.mktime(d1b.timetuple())
+        delta_p2 = p2 - p1
+
+        self.a2 = delta_p2/delta_d2
+        self.b2 = p1 - d1b*self.a2
+
     def getOpr(self, dzien:dt.timestamp):
-        pass
+
+
+        if dzien >= self.d0 and dzien < self.d1a:
+            dx = time.mktime(dzien.timetuple())
+            return self.a1*dx + self.b1
+        elif dzien >= self.d1a and dzien < self.d1b:
+            return self.p1
+        elif dzien >= self.d1b < self.d2:
+            dx = time.mktime(dzien.timetuple())
+            return self.a2*dx + self.b2
+        elif dzien > self.d2:
+            return self.p2
+        else:
+            raise Exception("dzien spoza przedzialu")
 
 def generate(plik, kapital, oprocentowanie, okresy, start_date):
 
