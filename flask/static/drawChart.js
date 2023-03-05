0c073852a8bdbb5407c18ee8eac7132406ffc76a
szalaj <mszalajski@gmail.com>
2023-03-05 12:46:44 +0100

    wykres rat

--- flask/static/drawChart.js
+++ flask/static/drawChart.js
@@ -19,13 +19,13 @@ dane_wykres2.forEach(function (d) {
 
 });
 
-var width_docs = document.getElementById('wykres').clientWidth;
+var width_docs = document.getElementById('wykres_kapital').clientWidth;
 
 var margin = { top: 60, right: 30, bottom: 70, left: 80 },
   width = width_docs - margin.left - margin.right,
   height = 450 - margin.top - margin.bottom;
 
-var svg = d3.select("#wykres")
+var svg = d3.select("#wykres_kapital")
   .append("svg")
   .attr("class", "svg-holder")
   .attr("width", width + margin.left + margin.right)
@@ -129,30 +129,122 @@ svg.append("text")
 
 
 
-function resize() {
-  width_docs = document.getElementById('wykres').clientWidth;
-
-  d3.select('#wykres svg')
-    .attr('width', width_docs)
-
-  xscale.range([0, width_docs - margin.left - margin.right]);
 
 
-  xAxis.call(d3.axisBottom(xscale).tickFormat(d3.timeFormat('%Y-%m-%d')))
 
-  grids.attr("x1", function (d) { return xscale(d) })
-  grids.attr("x2", function (d) { return xscale(d) })
+var svg2 = d3.select("#wykres_raty")
+  .append("svg")
+  .attr("class", "svg-holder")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .attr("style", "border:1px solid black;")
+  .style('background', "white")
+  .append("g")
+  .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
 
-  kreska.attr("d", d3.line()
+
+    var xAxis3 = svg2.append("g")
+  .attr("class", "xAxis")
+  .attr("transform", "translate(0," + height + ")")
+
+xAxis3.call(d3.axisBottom(xscale).tickFormat(d3.timeFormat('%Y-%m-%d')))
+  .selectAll("text")
+  .attr("transform", "translate(-10,0)rotate(-45)")
+  .style("text-anchor", "end")
+  .style("font-size", "13px");
+
+  var grids2 = svg2.append('g')
+  .selectAll('line')
+  .data(xscale.ticks())
+  .enter().append('line')
+  .attr('class', 'gridline')
+  .attr('x1', d => xscale(d))
+  .attr('x2', d => xscale(d))
+  .attr('y1', 0)
+  .attr('y2', height)
+  .attr('stroke', 'black')
+  
+
+    var maxYvalue2 = d3.max(dane_wykres, function (d) { return d.rata });
+    var minYvalue2 = d3.min(dane_wykres, function (d) { return d.rata });
+    
+    
+    
+    var yscale2 = d3.scaleLinear()
+      .domain([0, maxYvalue2])
+      .range([height, 0]);
+    
+
+
+      svg2.append("g")
+      .attr("class", "yAxis")
+      .call(d3.axisLeft(yscale2).tickFormat(formatMoney))
+      .style("font-size", "13px");
+
+    // var mybars = svg2.selectAll(".mybar").data(dane_wykres)
+
+    // mybars
+    //   .enter()
+    //   .append("circle")
+    //   .attr("cx", function (d) { return xscale(d.dzien); })
+    //   .attr("cy", function (d) { return yscale2(d.rata); })
+    //   .attr("r", 2)
+    //   .attr("class", "mybar");
+
+
+      var kreska3 = svg2.append("path")
+      .datum(dane_wykres)
+      .attr("class", "kreska")
+      .attr("d", d3.line()
+        .x(function (d) { return xscale(d.dzien) })
+        .y(function (d) { return yscale2(d.rata) })
+      )
+
+
+      
+  var kreska4 = svg2.append("path")
+  .datum(dane_wykres2)
+  .attr("class", "kreska2")
+  .attr("d", d3.line()
     .x(function (d) { return xscale(d.dzien) })
-    .y(function (d) { return yscale(d.K_po) })
+    .y(function (d) { return yscale2(d.rata) })
   )
 
-  kreska2.attr("d", d3.line()
-    .x(function (d) { return xscale(d.dzien) })
-    .y(function (d) { return yscale(d.K_po) })
-  )
 
-}
+  
+svg2.append("text")
+.attr("class", "text-tytul")
+.attr("x", 0)
+.attr("y", -24)
+.text("Raty")
 
-window.addEventListener('resize', resize);
+
+
+    function resize() {
+      width_docs = document.getElementById('wykres').clientWidth;
+    
+      d3.select('#wykres svg')
+        .attr('width', width_docs)
+    
+      xscale.range([0, width_docs - margin.left - margin.right]);
+    
+    
+      xAxis.call(d3.axisBottom(xscale).tickFormat(d3.timeFormat('%Y-%m-%d')))
+    
+      grids.attr("x1", function (d) { return xscale(d) })
+      grids.attr("x2", function (d) { return xscale(d) })
+    
+      kreska.attr("d", d3.line()
+        .x(function (d) { return xscale(d.dzien) })
+        .y(function (d) { return yscale(d.K_po) })
+      )
+    
+      kreska2.attr("d", d3.line()
+        .x(function (d) { return xscale(d.dzien) })
+        .y(function (d) { return yscale(d.K_po) })
+      )
+    
+    }
+    
+    window.addEventListener('resize', resize);
