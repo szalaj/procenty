04a8d25eb6f8b3f9133a6c8a53b7a2592e4a4336
szalaj <mszalajski@gmail.com>
2023-08-15 17:47:41 +0200

    backup

--- services/web/project/routes/admin.py
+++ services/web/project/routes/admin.py
@@ -1,8 +1,11 @@
 
 from flask import Blueprint, render_template, flash, redirect, url_for, request, send_file, sessions
 from flask_login import login_user, logout_user, login_required, current_user
-from ..models import User, Dom, Zapytanie
+from ..models import User, Dom, Zapytanie, InflacjaMM, Kredyt, Nadplata
 from project import db
+import os
+import csv
+
 admin_bp = Blueprint('admin_bp', __name__)
 
 @admin_bp.route('/login', methods=['GET', 'POST'])
@@ -26,3 +29,59 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('bp.main'))
+
+
+@admin_bp.route('/backup')
+@login_required
+def backup():
+    kredyty = [k.as_dict() for k in Kredyt.query.all()]
+    nadplaty = [k.as_dict() for k in Nadplata.query.all()]
+
+    
+
+    data_file = open('backup/kredyty_backup.csv', 'w')
+ 
+    # create the csv writer object
+    csv_writer = csv.writer(data_file)
+    
+    # Counter variable used for writing
+    # headers to the CSV file
+    count = 0
+    
+    for emp in kredyty:
+        if count == 0:
+    
+            # Writing headers of CSV file
+            header = emp.keys()
+            csv_writer.writerow(header)
+            count += 1
+    
+        # Writing data of CSV file
+        csv_writer.writerow(emp.values())
+    
+    data_file.close()
+
+    data_file = open('backup/nadplaty_backup.csv', 'w')
+ 
+    # create the csv writer object
+    csv_writer = csv.writer(data_file)
+    
+    # Counter variable used for writing
+    # headers to the CSV file
+    count = 0
+    
+    for emp in nadplaty:
+        if count == 0:
+    
+            # Writing headers of CSV file
+            header = emp.keys()
+            csv_writer.writerow(header)
+            count += 1
+    
+        # Writing data of CSV file
+        csv_writer.writerow(emp.values())
+    
+    data_file.close()
+    flash('backup zrobiony')
+    return redirect(url_for('dom.pokaz_kredyty'))
+
