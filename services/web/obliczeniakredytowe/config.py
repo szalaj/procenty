fb44f95c18db216da2525f6b9ffd35c4d405eb8e
szalaj <mszalajski@gmail.com>
2023-09-13 20:06:38 +0200

    db outside

--- services/web/obliczeniakredytowe/config.py
+++ services/web/obliczeniakredytowe/config.py
@@ -2,13 +2,15 @@ import os
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+print(basedir)
+dbpath = os.path.abspath(os.path.join(basedir, '../../db/database.db'))
+print(dbpath)
 
 class Config(object):
     STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/obliczeniakredytowe/static"
     MEDIA_FOLDER = f"{os.getenv('APP_FOLDER')}/obliczeniakredytowe/media"
     SECRET_KEY = 'asdfal3l3j4lkjlaksd333'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + dbpath
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SCHEDULER_API_ENABLED = True
     SCHEDULER_TIMEZONE = "Europe/Berlin"
