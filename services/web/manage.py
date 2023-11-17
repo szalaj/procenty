408fd0ba9904867294fbb2843f0c5fd55608cbfb
szalaj <mszalajski@gmail.com>
2023-11-17 22:41:02 +0100

    sciezka poprawiona

--- services/web/manage.py
+++ services/web/manage.py
@@ -6,9 +6,11 @@ from obliczeniakredytowe import init_app
 if 0:
     os.environ['APPDB_PATH'] = "/home/ubuntu/data/" 
 else:
+    print('starcik lokal')
     # test mode with gunicorn
     current_path = os.path.dirname(os.path.abspath(__file__))
-    db_path = os.path.abspath(os.path.join(current_path, '../../db'))
+    db_path = os.path.abspath(os.path.join(current_path, '../db'))
+    print(db_path)
     os.environ['APPDB_PATH'] = db_path 
 
 app = init_app()
