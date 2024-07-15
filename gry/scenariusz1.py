22287734b9bb13e67f6e90a7a4c1c24920c364e1
szalaj <mszalajski@gmail.com>
2024-07-15 22:46:07 +0200

    zasoby

---
+++ gry/scenariusz1.py
@@ -0,0 +1,5 @@
+
+# 1. Przydziel zasoby
+# 2 (pętla decyzyjna). Wyprodukuj zasoby, wymień zasoby
+# 3. Skonsumuj zasoby
+# 4. Jeśli zostałem zabity lub jestem jedynym żywym, zakończ. Jeśli nie, wróć do 2.
