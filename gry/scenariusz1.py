0275d88cfad13f9da03fbc5cfd4dfff4e91f0bd3
szalaj <mszalajski@gmail.com>
2024-07-16 20:55:36 +0200

    gry

--- gry/scenariusz1.py
+++ gry/scenariusz1.py
@@ -3,3 +3,17 @@
 # 2 (pętla decyzyjna). Wyprodukuj zasoby, wymień zasoby
 # 3. Skonsumuj zasoby
 # 4. Jeśli zostałem zabity lub jestem jedynym żywym, zakończ. Jeśli nie, wróć do 2.
+
+
+from procenty.symulacja.panstwo import Panstwo
+
+
+if __name__ == '__main__':
+    polska = Panstwo(28, 100,200,300)
+    niemcy = Panstwo(80, 200,300,100)
+    polska.polacz(niemcy)
+
+    if polska.sila > niemcy.sila:
+        print("Polska wygrała")
+    else:
+        print("Niemcy wygrały")
