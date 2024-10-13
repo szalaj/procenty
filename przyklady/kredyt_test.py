e896396f0322beaba287e36b3d44d07c5d9cd1bd
szalaj <mszalajski@gmail.com>
2024-10-13 18:25:46 +0200

    przeplywy

--- przyklady/kredyt_test.py
+++ przyklady/kredyt_test.py
@@ -3,26 +3,37 @@ from datetime import datetime
 from decimal import Decimal, ROUND_HALF_UP
 from dateutil.relativedelta import relativedelta
 from tabulate import tabulate
+import networkx as nx
+from procenty.podmioty import Podmiot, Menadzer
+k = Kredyt(Decimal(400000), 420, Decimal(0.076), Decimal(0.04), datetime(2021, 10, 1), 'rowne')
 
-
-
-k = Kredyt(Decimal(10000), 12, Decimal(0.03), Decimal(0.01), datetime(2020, 12, 11), 'rowne')
-
-
-
-datetimes = [datetime(2021, 1, 1) + relativedelta(months=i) for i in range(20)]
+datetimes = [datetime(2021, 11, 1) + relativedelta(months=i) for i in range(420)]
 for dzien_splaty in datetimes:
     k.zdarzenia.append(Zdarzenie(dzien_splaty, Rodzaj.SPLATA, 0))
 
 wynik = k.symuluj()
-# print(wynik['raty'][0].keys())
+
 
 headers = wynik['raty'][0].keys()
 rows = [list(item.values()) for item in wynik['raty']]
 
-# Print the table
-print(tabulate(rows, headers=headers, tablefmt="grid"))
-# R = []
-# for r in wynik['raty']:
-#     R.append(r['rata'])
-# print(R)
+
+mg = Menadzer('2022-10')
+
+p1 = Podmiot('2022-10','A', mg)
+p2 = Podmiot('2022-10','B', mg)
+
+mg.dodaj_polaczenie(p1, p2, typ='polaczenie')
+
+mg.dodaj_przeplyw(p2, p1, typ='przeplyw', czas='2022-10', kwota=400000)
+
+for r in rows:
+    rata=r[6]
+    kiedy=r[0]
+
+    mg.dodaj_przeplyw(p1, p2, typ='przeplyw', czas=kiedy, kwota=rata)
+
+mg.sim()
+
+print(p1.konto)
+print(p2.konto)
--- src/procenty/podmioty.py
+++ src/procenty/podmioty.py
@@ -1,6 +1,112 @@
 import numpy as np
 import pandas as pd
 from dataclasses import dataclass
+import networkx as nx
+
+
+
+
+@dataclass
+class Menadzer:
+    start: str
+    czas: int = 0
+    def __post_init__(self):
+        # check if start is a valid date
+        try:
+            self._start = pd.to_datetime(self.start)
+        except:
+            raise ValueError('Niepoprawna data')
+        
+        self._max_period_przeplyw = None
+        
+        self.g = nx.MultiDiGraph()
+        
+    def dodaj_polaczenie(self, podmiot1, podmiot2, typ):
+        self.g.add_edge(podmiot1, podmiot2, typ=typ)
+        
+    def dodaj_przeplyw(self, podmiot1, podmiot2, typ, czas, kwota):
+        try:
+            czas = pd.to_datetime(czas)
+        except:
+            raise ValueError('Niepoprawna data')
+        
+        if self._max_period_przeplyw is None:
+            self._max_period_przeplyw = czas
+        else:
+            if czas > self._max_period_przeplyw:
+                self._max_period_przeplyw = czas
+                
+        self.g.add_edge(podmiot1, podmiot2, typ=typ, kwota=kwota, kiedy=czas)
+    
+    def __repr__(self):
+        return f'Menadzer: {self.start}'
+    
+    @property
+    def rokmiesiac(self):
+        return self._start.to_period('M')
+     
+    @property
+    def max_period_przeplyw(self):
+        return self._max_period_przeplyw
+
+    def sim(self, n=None):
+        max_n = self.max_period_przeplyw
+        
+        start_n = self._start.to_period('M')
+        while start_n < max_n.to_period('M'):
+
+            for podmiot1, podmiot2, data in self.g.edges(data=True):
+                if data['typ'] == 'przeplyw':
+                    if data['kiedy'].to_period('M') == start_n:
+                        podmiot2.konto.ma += float(data['kwota'])
+                        podmiot1.konto.ma -= float(data['kwota'])
+
+
+            start_n = start_n + 1
+
+
+
+
+@dataclass
+class Konto:
+    winien: float
+    ma: float
+    
+    @property
+    def saldo(self):
+        return self.ma - self.winien
+    
+    def __repr__(self):
+        return f'Konto: {self.saldo}, winien: {self.winien}, ma: {self.ma}'
+
+
+'''Stan posiadania Podmiotu może się zmieniać co miesiąc.
+Podmiot ma konto, które jest zawsze z nim związane.
+
+'''
+@dataclass
+class Podmiot:
+    start: str
+    nazwa: str
+    mng: object
+    def __post_init__(self):
+        # check if start is a valid date
+        try:
+            self._start = pd.to_datetime(self.start)
+        except:
+            raise ValueError('Niepoprawna data')
+        
+        if self._start.to_period('M') != self.mng.rokmiesiac:
+            raise ValueError('Data niezgodna z menadżerem.')       
+        
+        self.konto = Konto(0, 0)
+    def __hash__(self):
+        return hash(self.nazwa)
+    def __repr__(self):
+        return f'Podmiot: {self.nazwa}'
+
+
+
 
 
 @dataclass
