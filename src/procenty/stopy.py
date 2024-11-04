e3dd9014b9c6768b0cba055e6bb18e69fa57effc
szalaj <mszalajski@gmail.com>
2024-11-04 21:40:44 +0100

    splajn

--- src/procenty/stopy.py
+++ src/procenty/stopy.py
@@ -4,3 +4,54 @@
 3. krzywą można interpolować
 '''
 
+from typing import List, Tuple
+from datetime import datetime
+from dataclasses import dataclass
+import numpy as np
+from scipy.interpolate import CubicSpline, interp1d
+from dateutil.relativedelta import relativedelta
+
+@dataclass
+class Krzywa:
+
+    punkty: List[Tuple[datetime, float]]
+
+    def __post_init__(self):
+        
+        self.punkty.sort(key=lambda x: x[0])
+
+        self.start = self.punkty[0][0]
+        self.end = self.punkty[-1][0]
+
+        self.timestamps = np.array([dt.timestamp() for dt, _ in self.punkty])
+        self.values = np.array([val for _, val in self.punkty])
+
+        spline = CubicSpline(self.timestamps, self.values)
+
+        # Generate points for smooth plot
+        self.timestamps_smooth = np.linspace(self.timestamps.min(), self.timestamps.max(), 500)
+        self.values_smooth = spline(self.timestamps_smooth)
+        
+        self.interpolation_function = interp1d(self.timestamps_smooth, self.values_smooth)
+
+ 
+    def __repr__(self):
+        return f'Krzywa({self.start}, {self.end}, {self.punkty})'
+    
+    @property
+    def splajn(self):
+        return list(zip(self.timestamps_smooth, self.values_smooth))
+    
+    @property
+    def punkty_zip(self):
+        return list(zip(self.timestamps, self.values))
+    
+    def podzial(self, okres_dni:int) -> list:
+        '''
+        Dzieli krzywą na okresy o długości okres_dni
+        '''
+
+        dajs = [self.start + relativedelta(days=(i+1)*okres_dni) for i in range(int((self.end - self.start).days/okres_dni))]
+        opr = [(d,self.interpolation_function(d.timestamp()).item()) for d in dajs]
+
+        return opr
