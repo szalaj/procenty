736b284132850736ca15ce859ac0e1aa4d7fbea7
szalaj <mszalajski@gmail.com>
2025-01-06 13:34:32 +0100

    przyklad gospodarka

--- src/procenty/gospodarka.py
+++ src/procenty/gospodarka.py
@@ -1,6 +1,11 @@
 import numpy as np
 from scipy.optimize import fsolve
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
+from procenty.stopy import Krzywa
+from procenty.kredyt import Kredyt, KredytPorownanie, Zdarzenie, Rodzaj, create_kredyt
+from decimal import Decimal
 
 k = 1.8
 beta = 1000
@@ -24,6 +29,29 @@ def oblicz():
 
     return Y_eq, r_eq
 
-for i in range(1, 10):
-    Pc = i
-    print(oblicz())
+start_p = datetime.strptime('2021-10-13', '%Y-%m-%d')
+lista_r = []
+for i in range(1, 500):
+    Pc = i/2.0
+    kol = start_p + relativedelta(months=(i))
+    Y, r = oblicz()
+    lista_r.append((kol, r))
+
+
+kr = Krzywa(lista_r)
+
+K = Decimal(400000)
+N = 300
+p1 = Decimal(0.02)
+p2 = p1*2
+marza = Decimal(0.1)
+start = datetime(2021, 10, 13)
+rodzaj = 'rowne'
+
+
+oprocentowania = [Zdarzenie(d[0],Rodzaj.OPROCENTOWANIE,d[1]) for d in kr.podzial(30)]
+zdarzenia = []
+zdarzenia.extend(oprocentowania)
+
+k4 = Kredyt(K, N, p1, marza, start, rodzaj, True, zdarzenia)
+print(k4.podsumowanie)
